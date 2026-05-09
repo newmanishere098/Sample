5a8d0a290e36b49e73150ae7efe7926669d45708
Shamnad <shamnad@gmail.com>
2026-05-09 19:32:04 +0530

    testtsnew

--- samplets.ts
+++ samplets.ts
@@ -1,39 +1,18 @@
 import express from "express";
-import sqlite3 from "sqlite3";
+import { exec } from "child_process";
 
 const app = express();
-app.use(express.json());
 
-const db = new sqlite3.Database(":memory:");
+app.get("/ping", (req, res) => {
+  const host = req.query.host as string;
 
-db.run(`
-  CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    username TEXT,
-    password TEXT
-  )
-`);
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  // ❌ VULNERABLE CODE
-  const query = `
-    SELECT * FROM users
-    WHERE username = '${username}'
-    AND password = '${password}'
-  `;
-
-  db.get(query, (err, row) => {
+  // ❌ VULNERABLE
+  exec(`ping -c 1 ${host}`, (err, stdout, stderr) => {
     if (err) {
-      return res.status(500).send(err.message);
+      return res.status(500).send(stderr);
     }
 
-    if (row) {
-      res.send("Logged in!");
-    } else {
-      res.status(401).send("Invalid credentials");
-    }
+    res.send(stdout);
   });
 });
 
